75c6e8f633c6a26fe819a4ca86664597af624522
Zack Gomez <zack@zack-desktop-ubuntu.(none)>
2010-07-05 22:32:49 -0700

    Now prints a random 6 cards.
    Also reorganized a bit, moving towards some sort of game integration.

--- guimain.cpp
+++ guimain.cpp
@@ -5,41 +5,37 @@
 #include <string>
 #include <iostream>
 #include <cassert>
+#include <vector>
+#include <ctime>
+#include "Deck.h"
+#include "Card.h"
 
 using namespace std;
+
+void initGL();
+void render();
 
 GLuint loadTexture(string filename);
 void drawCard(int row, int col);
 
+GLuint cardtex = 0;
+vector<Card> playedCards;
+
 int main(int argc, char **argv)
 {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+    srand(time(NULL));
 
-    SDL_SetVideoMode(800, 600, 32, SDL_OPENGL);
+    Deck deck;
+    deck.shuffle();
+    playedCards = deck.deal(6);
 
-    GLuint cardtex = loadTexture("resources/cards3.png");
-
-    glViewport(0, 0, 800, 600);
-    glMatrixMode(GL_PROJECTION);
-    glOrtho(0, 800, 600, 0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glClearColor(0, 1, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glEnable(GL_TEXTURE_RECTANGLE);
-    glBindTexture(GL_TEXTURE_RECTANGLE, cardtex);
-
-    glScalef(100, 155, 0);
-    drawCard(12,2);
-
-    SDL_GL_SwapBuffers();
-
+    initGL();
     SDL_Event e;
     bool cont = true;
     while (cont)
     {
+        render();
+
         while (SDL_PollEvent(&e))
         {
             switch(e.type)
@@ -50,11 +46,72 @@ int main(int argc, char **argv)
                 cont = false; break;
             }
         }
+
+        SDL_Delay(10);
     }
 
     SDL_Quit();
     return 0;
 }
+
+void initGL()
+{
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+
+    SDL_SetVideoMode(800, 600, 32, SDL_OPENGL);
+
+    glEnable(GL_TEXTURE_RECTANGLE);
+    cardtex = loadTexture("resources/cards3.png");
+
+    glViewport(0, 0, 800, 600);
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(0, 800, 600, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glClearColor(0, 0.5, 0, 1);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+};
+
+void render()
+{
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glEnable(GL_TEXTURE_RECTANGLE);
+    glBindTexture(GL_TEXTURE_RECTANGLE, cardtex);
+
+    glScalef(70, 94, 0);
+    for (int i = 0; i < playedCards.size(); i++)
+    {
+        glColor3f(1,1,1);
+
+        int col = playedCards[i].getNum();
+        col = (col == Card::ACE) ? 0 : col-1;
+        int row;
+        switch (playedCards[i].getSuit())
+        {
+        case Card::clubs:    row = 0; break;
+        case Card::diamonds: row = 1; break;
+        case Card::hearts:   row = 2; break;
+        case Card::spades:   row = 3; break;
+        }
+
+        cout << "Card: " << playedCards[i] << " row: " << row << " col: " << col << '\n';
+
+        drawCard(row, col);
+
+        glTranslatef(1.2, 0, 0);
+    }
+    cout << "----------------\n";
+
+    SDL_GL_SwapBuffers();
+}
+
 
 GLuint loadTexture(string filename)
 {
@@ -92,18 +149,17 @@ GLuint loadTexture(string filename)
 
 void drawCard(int row, int col)
 {
-    glColor3f(1,1,1);
     glBegin(GL_QUADS);
-        glTexCoord2i(row * 79, col * 123);
+        glTexCoord2i(col * 79, row * 123);
         glVertex3f(0, 0, 0);
 
-        glTexCoord2i(row * 79, (col+1) * 123);
+        glTexCoord2i(col * 79, (row+1) * 123);
         glVertex3f(0, 1, 0);
 
-        glTexCoord2i((row+1) * 79, (col+1) * 123);
+        glTexCoord2i((col+1) * 79, (row+1) * 123);
         glVertex3f(1, 1, 0);
 
-        glTexCoord2i((row+1) * 79, col * 123);
+        glTexCoord2i((col+1) * 79, row * 123);
         glVertex3f(1, 0, 0);
     glEnd();
 }
