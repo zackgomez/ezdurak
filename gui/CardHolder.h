4655b9802f0820ab4f46654c25c4b6ce70017e7e
zack <zacharyg@caltech.edu>
2010-08-11 13:16:46 -0700

    Added CardHolder.h file.
    This should have been included before.

---
+++ gui/CardHolder.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "core/Card.h"
+
+class CardHolder
+{
+public:
+    virtual ~CardHolder() { /* Empty */ }
+
+    virtual void addCard(Card c) = 0;
+    virtual void addCards(const std::vector<Card>& cs) = 0;
+    virtual void removeCard(Card c) = 0;
+};
+
