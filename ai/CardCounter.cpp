280ec9fefeb24dac6faac2a1eddf3a8a77a5e4c8
Zack Gomez <zacharyg@caltech.edu>
2010-09-06 21:56:07 -0700

    Added basic CardCounter class.

---
+++ ai/CardCounter.cpp
@@ -0,0 +1,80 @@
+#include "CardCounter.h"
+#include "core/GameAgent.h"
+
+CardCounter::CardCounter()
+{
+}
+
+CardCounter::~CardCounter()
+{
+}
+
+bool CardCounter::isDiscareded(const Card& c) const
+{
+    return discardedCards_.find(c) != discardedCards_.end();
+}
+
+
+void CardCounter::gameStart(GameAgent *agent)
+{
+    agent_ = agent;
+
+    std::vector<PlayerPtr> players = agent_->getPlayers();
+    for (int i = 0; i < players.size(); i++)
+    {
+        playerCards_[players[i]] = cardset();
+    }
+}
+
+void CardCounter::gameOver(const ConstPlayerPtr biscuit)
+{
+    // Ignored
+}
+    
+void CardCounter::newRound(const ConstPlayerPtr attacker, const ConstPlayerPtr defender)
+{
+    attacker_ = attacker;
+    defender_ = defender;
+    playedCards_.clear();
+}
+    
+void CardCounter::attackerPassed(const ConstPlayerPtr newAttacker)
+{
+    attacker_ = newAttacker;
+}
+    
+void CardCounter::endRound(bool successful)
+{
+    if (successful)
+        discardedCards_.insert(playedCards_.begin(), playedCards_.end());
+}
+    
+void CardCounter::attackingCard(const Card &c)
+{
+    playerCards_[attacker_].erase(c);
+}
+    
+void CardCounter::defendingCard(const Card &c)
+{
+    playerCards_[defender_].erase(c);
+}
+    
+void CardCounter::piledOnCard(const Card &c)
+{
+    playerCards_[attacker_].erase(c);
+}
+    
+void CardCounter::playedOut(ConstPlayerPtr p)
+{
+    // Ignored
+}
+    
+void CardCounter::givenCards(ConstPlayerPtr p, int n)
+{
+    // TODO check for last card added, if so, add the trumpCard
+}
+    
+void CardCounter::givenCards(ConstPlayerPtr player, const std::vector<Card>& cs)
+{
+    playerCards_[player].insert(cs.begin(), cs.end());
+}
---
+++ ai/CardCounter.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "core/GameListener.h"
+#include <set>
+#include <map>
+
+/** 
+ * This is a utility class that 'counts cards'.  It keeps track of cards that
+ * have been discarded or given to players.  It provides facilities for querying
+ * the gathered information as well as getting to the raw data.
+ */
+class CardCounter :
+    public GameListener
+{
+public:
+    CardCounter();
+    ~CardCounter();
+
+    typedef std::set<Card> cardset;
+
+    /** 
+     * Returns true if the passed card is in the discard pile.
+     * 
+     * @param c Card to check
+     * 
+     * @return True if the card is dead.
+     */
+    bool isDiscarded(const Card& c) const;
+    /** 
+     * Returns a list of the known cards a player has.  This information is
+     * gathered when a player unsuccessfully defends and takes known cards.
+     * It is updated when a player plays a card.
+     * 
+     * @param p The player to query against.
+     * 
+     * @return The known cards for that player
+     */
+    cardset knownCards(ConstPlayerPtr p) const;
+
+    // Methods from GameListener interface
+    void gameStart(GameAgent *agent);
+    void gameOver(const ConstPlayerPtr biscuit);
+    void newRound(const ConstPlayerPtr, const ConstPlayerPtr);
+    void attackerPassed(const ConstPlayerPtr);
+    void endRound(bool);
+    void attackingCard(const Card &);
+    void defendingCard(const Card &);
+    void piledOnCard(const Card &);
+    void playedOut(ConstPlayerPtr);
+    void givenCards(ConstPlayerPtr, int);
+    void givenCards(ConstPlayerPtr, const std::vector<Card>&);
+
+private:
+    cardset playedCards_;
+    cardset discardedCards_;
+    std::map<ConstPlayerPtr, cardset> playerCards_;
+
+    ConstPlayerPtr attacker_, defender_;
+    GameAgent* agent_;
+};
+
